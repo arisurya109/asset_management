794c76c5841662ed9768fd52a38ea4dd0525d6ad
arisurya109 <arisurya2605@gmail.com>
2025-10-20 06:45:44 +0700

    Add view Preparation

--- lib/modules/operations/presentation/view/operation_view.dart
+++ lib/modules/operations/presentation/view/operation_view.dart
@@ -1,6 +1,7 @@
 import 'package:asset_management/core/core.dart';
 import 'package:asset_management/features/inventory/presentation/inventory_view.dart';
 import 'package:asset_management/features/migration/migration_export.dart';
+import 'package:asset_management/features/preparation/presentation/view/preparation_view.dart';
 import 'package:asset_management/features/registration/registration_export.dart';
 import 'package:asset_management/features/transfer/transfer_export.dart';
 import 'package:flutter/material.dart';
@@ -27,7 +28,11 @@ class OperationView extends StatelessWidget {
         'icon': Assets.iAssetManagement,
         'view': InventoryView(),
       },
-      {'title': 'Preparation', 'icon': Assets.iPreparation},
+      {
+        'title': 'Preparation',
+        'icon': Assets.iPreparation,
+        'view': PreparationView(),
+      },
       {'title': 'Counting', 'icon': Assets.iCount},
     ];
     return Padding(
